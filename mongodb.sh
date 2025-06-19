a5194dbf1d2a1518b19d49e2f4a31c71616bfb67
tharun <tharundadithota@gmail.com>
2025-06-19 11:01:08 +0530

    Instance ID creation

--- mongodb.sh
+++ mongodb.sh
@@ -33,7 +33,7 @@ VALIDATE(){
     fi
 }
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo # /mongo.repo can be given with any name of our choice but .repo is must
 VALIDATE $? "Copying MONGODB repo" # it can be either mongo.repo or mongodb.repo, but having .repo is mandatory.
 
 dnf install mongodb-org -y &>>$LOG_FILE      # Here we need to redirect the log files by using &>>$LOG_FILE
