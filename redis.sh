8b2bdcf99c4d507e5c1cf49445ff768752616d60
tharun <tharundadithota@gmail.com>
2025-06-21 11:14:36 +0530

    Instance ID creation

--- redis.sh
+++ redis.sh
@@ -35,11 +35,25 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling redis:7"
 
-dnf install redis -y 
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling redis"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Started Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME $N" | tee -a $LOG_FILE
