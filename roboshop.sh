78cf5dd01306cc071cba7b2d0ad42c17277cbe40
tharun <tharundadithota@gmail.com>
2025-06-21 07:01:19 +0530

    Instance ID creation

--- roboshop.sh
+++ roboshop.sh
@@ -8,18 +8,19 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 #INSTANCES=("$@")
 ZONE_ID="Z04142321DEUW88VF6DED" #go to hosted zones --> click HZ details --> copy HZ ID
 DOMAIN_NAME="tharun78daws84s.site" # replace with your domain.
-RECORD_NAME=
 
 #Now we can loop it
-for instance in ${INSTANCES[@]}
+for instance in $@
 do
     # if [ $instance ] #privateIPaddress will be used when if not equal to frontend
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0dbb4d1864e2681be --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text) #if this command executes, we'll get the instance id which will be stored inside the INSTANCE_ID variable
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0]. PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0]. PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
@@ -31,7 +32,7 @@ do
         ,"Changes": [{
          "Action"             : "UPSERT"
             ,"ResourceRecordSet" : {
-              "Name"             : "'$instance'.'$DOMAIN_NAME'"
+              "Name"             : "'$RECORD_NAME'"
              ,"Type"            : "A"
              ,"TTL"             : 1
              ,"ResourceRecords" : [{
