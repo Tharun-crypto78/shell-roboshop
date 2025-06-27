30890ba7de92a9a0e5dfc62b75189916d153b14d
tharun <tharundadithota@gmail.com>
2025-06-27 18:48:51 +0530

    Calling the Scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,79 +1,13 @@
 #!/bin/bash
 
-# At 1st we need the root access for all --> so we need to check the root access 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-# creating a variable here
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD # gives the pwd where the script is present
+source ./common.sh
+app_name=catalogue
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE 
-
-# Check the user has root privileges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #if you want to manually exit then give other than 0, upto 127
-else
-    echo "You are running with the root access" | tee -a $LOG_FILE
-fi
-
-# validate function takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE  # tee command --> adds single input to the multiple outputs to the screen and also to the file.
-    else 
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE 
-        exit 1
-    fi
-}
 # Following the commands present in the git repo catalogue documentation
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling nodejs:20"
- 
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs:20"
-
-id roboshop
-if [ $? -ne 0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
-else
-    echo -e "System user roboshop already created ... $Y SKIPPING $n"
-fi
-
-mkdir -p /app 
-VALIDATE $? "Creating app directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading  Catalogue"
-
-rm -rf /app/* # to remove the content present inside the app directory
-cd /app 
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzipping catalogue"
-
-npm install &>>LOG_FILE
-VALIDATE $? "Installing Dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service  #At present we're in the app location of the script --> which don't contain catalogue.service so it will fail --> If it have to should work irrelevant of wherever it is present then give "absolute path"
-VALIDATE $? "Copying catalogue service"
-
-systemctl daemon-reload &>>$LOG_FILE
-systemctl enable catalogue &>>$LOG_FILE
-systemctl start catalogue
-VALIDATE $? "Starting Catalogue"
+check_root
+app_setup
+nodejs_setup
+systemd_setup
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
@@ -88,3 +22,4 @@ else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
 
+print_time
