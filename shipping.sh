603509e89e1bf19bbf69d5b1968613a7ac5f9dcc
tharun <tharundadithota@gmail.com>
2025-06-27 11:41:51 +0530

    Mysql installation

--- shipping.sh
+++ shipping.sh
@@ -84,10 +84,16 @@ VALIDATE $? "Starting Shipping"
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h mysql.tharun78daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-mysql -h mysql.tharun78daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
-mysql -h mysql.tharun78daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
-VALIDATE $? "Loading the data into MySQL"
+$ mysql -h mysql.tharun78daws84s.site -u root -proboshop@1 -e 'use cities'
+if [ $? -ne 0 ]
+then
+    mysql -h mysql.tharun78daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.tharun78daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
+    mysql -h mysql.tharun78daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "Loading the data into MySQL"
+else
+    echo -e "Data is already laoded into MySQL ... $Y SKIPPING $N"
+fi
 
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart Shipping"
